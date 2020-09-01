307a9a20edde392c7678599cf182efbe1a6cd2ed
adrian.jazowski@gmail.com <adrian.jazowski@gmail.com>
2020-09-01 21:52:17 +0200

    Last commit with full responsible page

--- js/main.js
+++ js/main.js
@@ -7,16 +7,73 @@ const values = [
 
 const vaulesContainer = document.querySelector(".app--values");
 
-const desktopViewporst = window.matchMedia("screen and (min-width: 500px)");
+const desktopViewport = window.matchMedia("screen and (min-width: 500px)");
 
+const drawValues = (isDesktop) => {
+  if (isDesktop) {
+    drawDesktopValues();
+  } else {
+    drawMobileValues();
+  }
+};
+
+// draw list
+const drawMobileValues = () => {
+  vaulesContainer.innerHTML = "";
+
+  let list = document.createElement("ul");
+
+  values.forEach((value) => {
+    let element = document.createElement("li");
+    let name = document.createElement("div");
+    name.innerHTML = `<strong>Nazwa: </strong> ${value.name}`;
+
+    let calories = document.createElement("div");
+    calories.innerHTML = `<strong>Kalorie: </strong> ${value.calories}`;
+
+    let fat = document.createElement("div");
+    fat.innerHTML = `<strong>Tłuszcze: </strong> ${value.fat}`;
+
+    let carbs = document.createElement("div");
+    carbs.innerHTML = `<strong>Węglowodany: </strong> ${value.carbs}`;
+
+    element.appendChild(name);
+    element.appendChild(calories);
+    element.appendChild(fat);
+    element.appendChild(carbs);
+
+    list.appendChild(element);
+  });
+
+  vaulesContainer.appendChild(list);
+};
+
+// draw table
 const drawDesktopValues = () => {
   vaulesContainer.innerHTML = "";
 
   let table = document.createElement("table");
+
   let thead = document.createElement("thead");
   thead.innerHTML =
     "<tr><th>Nazwa</th><th>Kalorie</th><th>Tłuszcz</th><th>Węglowodany</th></tr>";
 
+  let tbody = document.createElement("tbody");
+
+  values.forEach((value) => {
+    let row = document.createElement("tr");
+    row.innerHTML = `<td>${value.name}</td><td>${value.calories}</td><td>${value.fat}</td><td>${value.carbs}</td>`;
+    tbody.appendChild(row);
+  });
+
   table.appendChild(thead);
+  table.appendChild(tbody);
+
   vaulesContainer.appendChild(table);
 };
+
+drawValues(desktopViewport.matches);
+
+desktopViewport.addListener((isDesktop) => {
+  drawValues(isDesktop.matches);
+});
